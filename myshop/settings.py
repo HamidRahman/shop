63a27b7088c668ba6eb685be31ed43fe84369d09
Hamid Rahman <waterbrain342@gmail.com>
2025-07-20 12:10:24 +0500

    Cart feature added

--- myshop/settings.py
+++ myshop/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # My Apps
+    'cart.apps.CartConfig',
     'shop.apps.ShopConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -64,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',  # Custom context processor for cart
             ],
         },
     },
